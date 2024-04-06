763dfc82e5073d5929038f64cbc1d8020deecff5
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-04-06 06:04:04 -0500

    🥷Add the Message component into the frontend project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import PrivateRoute from "./utils/PrivateRoute"
 import { AuthProvider } from './context/AuthContext'
 
@@ -10,16 +10,16 @@ import Loginpage from './views/Loginpage'
 import Dashboard from './views/Dashboard'
 import Navbar from './views/Navbar'
 import Todo from './views/Todo'
-
-
+import Message from './views/Message'
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        < Navbar/>
+        < Navbar />
         <Switch>
           <PrivateRoute component={Dashboard} path="/dashboard" exact />
+          <PrivateRoute component={Message} path="/inbox" exact />
           <Route component={Loginpage} path="/login" />
           <Route component={Registerpage} path="/register" exact />
           <Route component={Homepage} path="/" exact />
---
+++ frontend/src/views/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Message() {
+  return (
+    <div>Bannana</div>
+  );
+}
+
+export default Message;
+
