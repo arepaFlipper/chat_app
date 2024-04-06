00aedf78d78c6abe08b5e2f54c7c19e84acc8fd6
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-04-06 14:28:20 -0500

    💋 test the get-messages & my-messages endpoints

--- backend/test/my_messages.py
+++ backend/test/my_messages.py
@@ -2,7 +2,7 @@
 
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/get-messages/1/1/"
+endpoint = "http://127.0.0.1:8000/api/my-messages/1/"
 access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzEyODQ0MzM3LCJpYXQiOjE3MTI0MTIzMzcsImp0aSI6ImViYzg5MGY4ZjdhOTRmOGI5N2NkZGYwNzRjOWRlYTc5IiwidXNlcl9pZCI6MSwiZnVsbF9uYW1lIjoiIiwidXNlcm5hbWUiOiJyb290IiwiZW1haWwiOiJyb290QG1haWwuY29tIiwiYmlvIjoiIiwiaW1hZ2UiOiJkZWZhdWx0LmpwZyIsInZlcmlmaWVkIjpmYWxzZX0.hiOgbR0O2AT29K-pbO4pae5vYMaofaruS5u2IKikn4s"
 
 headers = {
