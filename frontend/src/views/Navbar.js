12389f97ccf95aa2319a7adb8c9f00593a4ed4de
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-04-06 06:13:39 -0500

    ☎️  include the Inbox route into the Navbar items

--- frontend/src/views/Navbar.js
+++ frontend/src/views/Navbar.js
@@ -1,24 +1,24 @@
-import {useContext} from 'react'
+import { useContext } from 'react'
 import jwt_decode from "jwt-decode"
 import AuthContext from '../context/AuthContext'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
 
-  const {user, logoutUser} = useContext(AuthContext)
+  const { user, logoutUser } = useContext(AuthContext)
   const token = localStorage.getItem("authTokens")
 
-  if (token){
-    const decoded = jwt_decode(token) 
+  if (token) {
+    const decoded = jwt_decode(token)
     var user_id = decoded.user_id
   }
 
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img style={{width:"120px", padding:"6px"}} src="https://i.imgur.com/juL1aAc.png" alt="" />
+            <img style={{ width: "120px", padding: "6px" }} src="https://i.imgur.com/juL1aAc.png" alt="" />
 
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,31 +29,34 @@ function Navbar() {
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              {token === null && 
-              <>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/login">Login</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/register">Register</Link>
-                </li>
-              </>
+              {token === null &&
+                <>
+                  <li class="nav-item">
+                    <Link class="nav-link" to="/login">Login</Link>
+                  </li>
+                  <li class="nav-item">
+                    <Link class="nav-link" to="/register">Register</Link>
+                  </li>
+                </>
               }
 
-            {token !== null && 
-              <>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/dashboard">Dashboard</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/todo">Todo</Link>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" onClick={logoutUser} style={{cursor:"pointer"}}>Logout</a>
-                </li>
-              </>
-              }   
-              
+              {token !== null &&
+                <>
+                  <li class="nav-item">
+                    <Link class="nav-link" to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li class="nav-item">
+                    <Link class="nav-link" to="/todo">Todo</Link>
+                  </li>
+                  <li class="nav-item">
+                    <Link class="nav-link" to="/inbox">Inbox</Link>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" onClick={logoutUser} style={{ cursor: "pointer" }}>Logout</a>
+                  </li>
+                </>
+              }
+
             </ul>
           </div>
         </div>
